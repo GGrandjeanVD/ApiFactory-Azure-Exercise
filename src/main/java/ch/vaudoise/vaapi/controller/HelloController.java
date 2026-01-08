95a40bb8b23863e2b1726a8a43a51b737e9ad0f9
Guillaume Grandjean <ggrandjean@vaudoise.ch>
2026-01-08 13:36:55 +0100

    First commit - create java project with controller

---
+++ src/main/java/ch/vaudoise/vaapi/controller/HelloController.java
@@ -0,0 +1,15 @@
+package ch.vaudoise.vaapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public Map<String, String> helloWorld() {
+        return Map.of("message", "hello-world");
+    }
+}
