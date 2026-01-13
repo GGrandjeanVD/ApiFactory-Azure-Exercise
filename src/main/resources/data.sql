4a6494afb24e2767fd7d67d953d26cf8e77f9eca
GGrandjeanVD <ggrandjean@vaudoise.ch>
2026-01-13 17:43:55 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS hello (
                                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                      message VARCHAR(255) NOT NULL
     );
